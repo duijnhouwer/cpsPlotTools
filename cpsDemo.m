b40b1ee8ff218729c523c38e0957bba12a29025d
duijnhouwer <j.duijnhouwer@gmail.com>
2016-07-13 13:36:55 -0400

    Added HTML demo page

--- cpsDemo.m
+++ cpsDemo.m
@@ -1,32 +1,42 @@
-function cpsDemo
-    
-    % Create some dummy data to plot ...
-    someData = mvnrnd([0;0], [1 .9;.9 1], 100);
-    x = someData(:,1);
-    y = someData(:,2);
-    % Open a figure window with the function name and clear it if necessary
-    for t = {'before','after'}
-        cpsFindFig(['cpsDemo - ' t{1}]);
-        % Create 42 subplots with some dummy data in m
-        subplot(2,2,1);
-        plot(x,y,'o');
-        subplot(2,2,2);
-        plot(x*2,y,'o');
-        h=subplot(2,2,3);
-        plot(x,y*2,'o');
-        xlabel('X (a.u.)','FontSize',12);
-        ylabel('Y (a.u.)','FontSize',12);
-        subplot(2,2,4);
-        plot(x*2,y*2,'o');
-    end
+%% cpsPlotTools
+% Demonstrations of the Capsaicin (CPS) Plot Tools.
 
-    % Unify the axis, between and within panels
-    cpsUnifyAxes
-    % Add reference axes
-  	cpsRefLine(gcf,'+','/','k--');
-    % Add label
-    cpsLabelPanels;
-    % Tile the before and after figures
-    cpsTileFigs
+%% Format multi panel scatter plots
+% Create 4 panels of bivariate dummy data with lettering to identify the
+% panels, scaled axes to aid comparison across panels, and added annotation
+% and reference lines.
+
+cpsFindFig('Ex1');               % Find or create a figure by name
+clf;
+for i=1:4
+    % Create a scatter plot
+    subplot(2,2,i,'align');
+    dumdat = mvnrnd([0;0],[i 1;1 i],50*i);
+    plot(dumdat(:,1),dumdat(:,2),'o');
+    cpsText(['N = ' num2str(50*i)] ... % Add annotation
+        ,'Location','BottomRight');  
 end
-    
+cpsLabelPanels;                   % Add lettering
+cpsUnifyAxes('within','between'); % Scale the axis limits
+cpsRefLine(gcf,'+','/','k--');    % Add dashed reference lines to all Axes
+
+
+%% Unify color axes to aid comparison
+% It is often desirable to scale the color axis limits of related data to
+% their combined global minimum and maximum to aid comparison.
+
+cpsFindFig('Ex2' ...                    % Open a Figure named Ex2 ...
+    ,'Position',[10 100 560 200]);      % ... with reduced height
+clf
+subplot(1,3,1); 
+contourf(peaks-50);
+axis square
+subplot(1,3,2); 
+contourf(peaks*4);
+axis square
+subplot(1,3,3);                         % Add a single ...  
+colorbar('West');                       % ... colorbar ...
+axis off equal square                   % ... for both panels.
+cpsUnifyAxes('C');                      % Scale the color axes              
+cpsLabelPanels('12 ' ...                % Number the first 2 panels ...
+    ,'Color','m','Location','inside');  % ... with some added style. 
